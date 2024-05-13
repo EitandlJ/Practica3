0802f801e2284cee843550fb6e685448048448ed
Eitan <eitandelajaille1@upb.edu>
2024-05-13 19:51:02 -0400

    Creacion del codigo del cliente

---
+++ Practica3/Practica3/Controllers/PatientController1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController1 : ControllerBase
+    {
+    }
+}
